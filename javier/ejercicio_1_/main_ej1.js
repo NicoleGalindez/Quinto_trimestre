c64e357cc938f8b8ab982c0ae8c1bcd3dc2b5545
NicoleGalindez <nbgalindez@misena.edu.co>
2023-08-02 17:37:56 -0500

    entrega ejercicio 2

--- javier/ejercicio_1_/main_ej1.js
+++ javier/ejercicio_1_/main_ej1.js
@@ -1,10 +1,39 @@
-      let email = document.querySelector("#email").value;
-      let password = document.querySelector("#password").value;
+      let email = document.querySelector('#email').value;
+      let password = document.querySelector('#password').value;
 
  
-      if (email === "admin@sena.com" && password === "12345678") {
-        document.body.style.backgroundColor = '#A5DDF9';
-      } 
-      else (email!= "admin@sena.com" && password != "12345678"){
-        document.body.style.backgroundColor = '#9566E0';
-      };
+      const enviar = document.querySelector('#enviar');
+      console.log(enviar);
+      
+      enviar.addEventListener('click',()=>{
+
+
+function inicio(){
+
+        if (email === "admin@sena.com" && password === "12345678"){
+          document.body.style.backgroundColor = '#A5DDF9';//azul claro
+          alert(`bienvenido ${email}` )
+        } 
+         else if (email!== "admin@sena.com" && password !== "12345678"){
+          document.body.style.backgroundColor = '#9566E0';// violeta    si funciona
+           alert(`bienvenido ${email}` )}
+
+          else if ( email=="nico@gmail.com" && password=="11111111"){
+            document.body.style.backgroundColor='#E0FED7';//verde claro
+          alert(`bienvenido ${email}`)
+
+        }  
+      //   else if ( email && password){
+      //     document.body.style.backgroundColor='#FFFED7';//amarillo claro
+      //   alert(`bienvenido ${email}`)
+      //  }  
+
+        else if( password.length <8){
+            alert("maximo 8 caracteres")
+        }
+    
+  
+
+      } inicio();
+
+    });
