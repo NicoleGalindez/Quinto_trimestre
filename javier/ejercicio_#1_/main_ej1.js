8b44b397b54cec39947feb92814c1316d05e59ed
NicoleGalindez <nbgalindez@misena.edu.co>
2023-08-01 23:19:02 -0500

    algo mas era

---
+++ javier/ejercicio_#1_/main_ej1.js
@@ -0,0 +1,9 @@
+      let email = document.getElementById("email").value;
+      let password = document.getElementById("password").value;
+
+ 
+      if (email === "admin@sena.com" && password === "12345678") {
+        document.body.style.backgroundColor = "#AED6F1";
+      } else {
+        document.body.style.backgroundColor = "#FFC0CB";
+      };
