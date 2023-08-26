4602187a91da5b4b714eb3d8d062a9cc6568765e
NicoleGalindez <nbgalindez@misena.edu.co>
2023-08-26 17:24:53 -0500

    falta local storage

--- javier/ejercicio_4_carrito/js/main.js
+++ javier/ejercicio_4_carrito/js/main.js
@@ -1,63 +1,13 @@
-/*  
-// MI CODIGO
-const card1= document.getElementById('card1');
 
-let contarclick=0;
-
-card1.addEventListener("click", () => {
-    contarclick++;
-    //numeroclicks();
-    numeroclicks();
-})
-
-
-
-// card 1
-const imagencard1=document.getElementById('imagencard1').src;
-const nombreproductocard1=document.getElementById('nombreproductocard1').textContent;
-const preciocard1=document.getElementById('preciocard1').textContent;
-
-
-const carritocompra=document.getElementById('carrito');
-
-
-function numeroclicks() {
-        const nuevafila1 = document.createElement('tr');
-        nuevafila1.innerHTML = `
-        <td><img src="${imagencard1}" alt="Imagen del producto"  width='80px'  > </td>
-                        <td>${nombreproductocard1}</td>
-                        <td>${preciocard1}</td>
-                        <td>${contarclick}</td>
-                        `;
-                        
-        carritocompra.appendChild(nuevafila1);
-        
-}
-
-
-console.log(imagencard1)
-console.log(nombreproductocard1)
-console.log(preciocard1)
-   */
-
-
-
-
-
-// CORRECCION DE CHAT
-
+const carritocompra = document.getElementById('carrito');
 
 
 // card 1-------------------------------------------------------------
-const carritocompra = document.getElementById('carrito');
 let contarclick = 0;
-
-
 let nuevafila = 0;
 function crearFila() {
     nuevafila = document.createElement('tr');
     carritocompra.appendChild(nuevafila);
-
 }
 
 function actualizarFila(imagen, nombre, precio) {
@@ -70,7 +20,6 @@ function actualizarFila(imagen, nombre, precio) {
 }
 
 crearFila(); // Crea la fila inicial
-
 
 // card1
 const card = document.querySelector('#card1');
@@ -85,9 +34,7 @@ card.addEventListener("click", () => {
 
 
 // card 2-------------------------------------------------------------
-
 let contarclick2 = 0;
-
 let nuevafila2 = 0;
 function crearFila2() {
     nuevafila2 = document.createElement('tr');
@@ -102,8 +49,7 @@ function actualizarFila2(imagen, nombre, precio) {
         <td>${contarclick2}</td>
     `;
 }
-
-crearFila2(); // Crea la fila 2
+crearFila2(); 
 
 // card2
 const card2 = document.querySelector('#card2');
@@ -112,14 +58,13 @@ card2.addEventListener("click", () => {
     const imagencard2 = document.querySelector('#imagencard2').src;
     const nombreproductocard2 = document.querySelector('#nombreproductocard2').textContent;
     const preciocard2 = document.querySelector('#preciocard1').textContent;
-    actualizarFila2(imagencard2, nombreproductocard2, preciocard2);
-    
+    actualizarFila2(imagencard2, nombreproductocard2, preciocard2);  
 });
+
 
 
 // card 3-------------------------------------------------------------
 let contarclick3 = 0;
-
 let nuevafila3 = 0;
 function crearFila3() {
     nuevafila3 = document.createElement('tr');
@@ -134,8 +79,7 @@ function actualizarFila3(imagen, nombre, precio) {
         <td>${contarclick3}</td>
     `;
 }
-
-crearFila3(); // Crea la fila 3
+crearFila3(); 
 
 // card3
 const card3 = document.querySelector('#card3');
@@ -144,16 +88,13 @@ card3.addEventListener("click", () => {
     const imagencard3 = document.querySelector('#imagencard3').src;
     const nombreproductocard3 = document.querySelector('#nombreproductocard3').textContent;
     const preciocard3 = document.querySelector('#preciocard3').textContent;
-    actualizarFila3(imagencard3, nombreproductocard3, preciocard3);
-    
+    actualizarFila3(imagencard3, nombreproductocard3, preciocard3);  
 });
-
 
 
 
 // card 4-------------------------------------------------------------
 let contarclick4 = 0;
-
 let nuevafila4 = 0;
 function crearFila4() {
     nuevafila4 = document.createElement('tr');
@@ -168,8 +109,7 @@ function actualizarFila4(imagen, nombre, precio) {
         <td>${contarclick4}</td>
     `;
 }
-
-crearFila4(); // Crea la fila 4
+crearFila4();
 
 // card4
 const card4 = document.querySelector('#card4');
@@ -178,17 +118,13 @@ card4.addEventListener("click", () => {
     const imagencard4 = document.querySelector('#imagencard4').src;
     const nombreproductocard4 = document.querySelector('#nombreproductocard4').textContent;
     const preciocard4 = document.querySelector('#preciocard4').textContent;
-    actualizarFila4(imagencard4, nombreproductocard4, preciocard4);
-    
+    actualizarFila4(imagencard4, nombreproductocard4, preciocard4);  
 });
-
-
 
 
 
 // card 5-------------------------------------------------------------
 let contarclick5 = 0;
-
 let nuevafila5 = 0;
 function crearFila5() {
     nuevafila5 = document.createElement('tr');
@@ -203,8 +139,7 @@ function actualizarFila5(imagen, nombre, precio) {
         <td>${contarclick5}</td>
     `;
 }
-
-crearFila5(); // Crea la fila 5
+crearFila5(); 
 
 // card5
 const card5 = document.querySelector('#card5');
@@ -214,15 +149,12 @@ card5.addEventListener("click", () => {
     const nombreproductocard5 = document.querySelector('#nombreproductocard5').textContent;
     const preciocard5 = document.querySelector('#preciocard5').textContent;
     actualizarFila5(imagencard5, nombreproductocard5, preciocard5);
-    
 });
 
 
 
 // card 6-------------------------------------------------------------
-// card 5-------------------------------------------------------------
 let contarclick6 = 0;
-
 let nuevafila6= 0;
 function crearFila6() {
     nuevafila6 = document.createElement('tr');
@@ -237,8 +169,8 @@ function actualizarFila6(imagen, nombre, precio) {
         <td>${contarclick6}</td>
     `;
 }
+crearFila6(); 
 
-crearFila6(); // Crea la fila 6
 // card 6
 const card6 = document.querySelector('#card6');
 card6.addEventListener("click", () => {
@@ -252,7 +184,6 @@ card6.addEventListener("click", () => {
 
 // card 7-------------------------------------------------------------
 let contarclick7 = 0;
-
 let nuevafila7 = 0;
 function crearFila7() {
     nuevafila7 = document.createElement('tr');
@@ -267,8 +198,7 @@ function actualizarFila7(imagen, nombre, precio) {
         <td>${contarclick7}</td>
     `;
 }
-
-crearFila7(); // Crea la fila 7
+crearFila7(); 
 
 // card7
 const card7 = document.querySelector('#card7');
@@ -277,15 +207,13 @@ card7.addEventListener("click", () => {
     const imagencard7 = document.querySelector('#imagencard7').src;
     const nombreproductocard7 = document.querySelector('#nombreproductocard7').textContent;
     const preciocard7 = document.querySelector('#preciocard7').textContent;
-    actualizarFila7(imagencard7, nombreproductocard7, preciocard7);
-    
+    actualizarFila7(imagencard7, nombreproductocard7, preciocard7);  
 });
 
 
 
 // card 8-------------------------------------------------------------
 let contarclick8 = 0;
-
 let nuevafila8 = 0;
 function crearFila8() {
     nuevafila8 = document.createElement('tr');
@@ -300,8 +228,7 @@ function actualizarFila8(imagen, nombre, precio) {
         <td>${contarclick8}</td>
     `;
 }
-
-crearFila8(); // Crea la fila 8
+crearFila8(); 
 
 // card8
 const card8 = document.querySelector('#card8');
@@ -311,15 +238,12 @@ card8.addEventListener("click", () => {
     const nombreproductocard8 = document.querySelector('#nombreproductocard8').textContent;
     const preciocard8 = document.querySelector('#preciocard8').textContent;
     actualizarFila5(imagencard8, nombreproductocard8, preciocard8);
-    
 });
-
 
 
 
 // card 9-------------------------------------------------------------
 let contarclick9 = 0;
-
 let nuevafila9 = 0;
 function crearFila9() {
     nuevafila9 = document.createElement('tr');
@@ -334,8 +258,7 @@ function actualizarFila9(imagen, nombre, precio) {
         <td>${contarclick9}</td>
     `;
 }
-
-crearFila9(); // Crea la fila 9
+crearFila9(); 
 
 // card9
 const card9 = document.querySelector('#card9');
@@ -345,15 +268,12 @@ card9.addEventListener("click", () => {
     const nombreproductocard9 = document.querySelector('#nombreproductocard9').textContent;
     const preciocard9 = document.querySelector('#preciocard9').textContent;
     actualizarFila9(imagencard9, nombreproductocard9, preciocard9);
-    
 });
 
 
 
 // card 10-------------------------------------------------------------
-
 let contarclick10 = 0;
-
 let nuevafila10 = 0;
 function crearFila10() {
     nuevafila10 = document.createElement('tr');
@@ -368,8 +288,7 @@ function actualizarFila10(imagen, nombre, precio) {
         <td>${contarclick10}</td>
     `;
 }
-
-crearFila10(); // Crea la fila 10
+crearFila10(); 
 
 // card10
 const card10 = document.querySelector('#card10');
@@ -379,15 +298,12 @@ card10.addEventListener("click", () => {
     const nombreproductocard10 = document.querySelector('#nombreproductocard10').textContent;
     const preciocard10 = document.querySelector('#preciocard10').textContent;
     actualizarFila10(imagencard10, nombreproductocard10, preciocard10);
-    
 });
 
 
 
 // card 11-------------------------------------------------------------
-
 let contarclick11 = 0;
-
 let nuevafila11 = 0;
 function crearFila11() {
     nuevafila11 = document.createElement('tr');
@@ -402,8 +318,7 @@ function actualizarFila11(imagen, nombre, precio) {
         <td>${contarclick11}</td>
     `;
 }
-
-crearFila11(); // Crea la fila 11
+crearFila11(); 
 
 // card11
 const card11 = document.querySelector('#card11');
@@ -413,15 +328,12 @@ card11.addEventListener("click", () => {
     const nombreproductocard11= document.querySelector('#nombreproductocard11').textContent;
     const preciocard11 = document.querySelector('#preciocard11').textContent;
     actualizarFila11(imagencard11, nombreproductocard11, preciocard11);
-    
 });
 
 
 
 // card 12-------------------------------------------------------------
-
 let contarclick12 = 0;
-
 let nuevafila12 = 0;
 function crearFila12() {
     nuevafila12 = document.createElement('tr');
@@ -436,8 +348,7 @@ function actualizarFila12(imagen, nombre, precio) {
         <td>${contarclick12}</td>
     `;
 }
-
-crearFila12(); // Crea la fila 12
+crearFila12(); 
 
 // card12
 const card12 = document.querySelector('#card12');
@@ -447,14 +358,13 @@ card12.addEventListener("click", () => {
     const nombreproductocard12= document.querySelector('#nombreproductocard12').textContent;
     const preciocard12 = document.querySelector('#preciocard12').textContent;
     actualizarFila12(imagencard12, nombreproductocard12, preciocard12);
-    
 });
 
 
-
-
-const vaciarCarritoBtn = document.getElementById('vaciarCarrito');
-vaciarCarritoBtn.addEventListener('click', () => {
+//------------------------------------------------------------------------
+// vaciar del carrito :
+const vaciarcarrito = document.getElementById('vaciarCarrito');
+vaciarcarrito.addEventListener('click', () => {
   
    carritocompra.removeChild(nuevafila);
    carritocompra.removeChild(nuevafila2);
@@ -468,5 +378,19 @@ vaciarCarritoBtn.addEventListener('click', () => {
    carritocompra.removeChild(nuevafila10);
    carritocompra.removeChild(nuevafila11);
    carritocompra.removeChild(nuevafila12);
- // falta completar , elimina pero no vuelve a agregar
+ // vuelve a agregar
+    crearFila(actualizarFila(contarclick=0));
+    crearFila2(actualizarFila2(contarclick2=0));
+    crearFila3(actualizarFila3(contarclick3=0));
+    crearFila4(actualizarFila4(contarclick4=0));
+    crearFila5(actualizarFila5(contarclick5=0));
+    crearFila6(actualizarFila6(contarclick6=0));
+    crearFila7(actualizarFila7(contarclick7=0));
+    crearFila8(actualizarFila8(contarclick8=0));
+    crearFila9(actualizarFila9(contarclick9=0));
+    crearFila10(actualizarFila10(contarclick10=0));
+    crearFila11(actualizarFila11(contarclick11=0));
+    crearFila12(actualizarFila12(contarclick12=0));
 });
+
+
